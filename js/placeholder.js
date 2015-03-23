6ef03efe11ae686b587f5d13f8e21bffd0743fd7
Nightpaws <nightpaws@hotmail.co.uk>
2015-03-23 20:49:53 +0000

    Placeholder Javascript
    Allowing the creation of the /js/ directory.

---
+++ js/placeholder.js
@@ -0,0 +1,8 @@
+/*
+This file is a spacefiller to allow GitHub to accept the folder structure.
+Once another JavaScript file exists, please delete this one! Thanks!
+
+*/
+function() null {
+	console.log("Null function called!");
+}
