42e9275ffc3e5ed8e85ea2771d322ca4a562123b
Nightpaws <nightpaws@hotmail.co.uk>
2015-04-14 22:14:59 +0100

    Population File
    Population file added for easy recreation of sample data. User table
    added.

---
+++ database/Queries/population.sql
@@ -0,0 +1,6 @@
+-- User Table Population
+INSERT INTO `Users`(`username`, `password`, `email`, `isAdmin`) VALUES ("nightpaws","pass123","hwb121279@uni.strath.ac.uk","1")
+INSERT INTO `Users`(`username`, `password`, `email`, `isAdmin`) VALUES ("ZlatanIbrahimovic","abc123","rqb12155@uni.strath.ac.uk","0")
+INSERT INTO `Users`(`username`, `password`, `email`, `isAdmin`) VALUES ("AGreatPigeon","password","trb12149@uni.strath.ac.uk","1")
+INSERT INTO `Users`(`username`, `password`, `email`, `isAdmin`) VALUES ("willton","passwd","tjb12161@uni.strath.ac.uk","0")
+INSERT INTO `Users`(`username`, `password`, `email`, `isAdmin`) VALUES ("shannonfitz","pwd1234","seb12173@uni.strath.ac.uk","0")
