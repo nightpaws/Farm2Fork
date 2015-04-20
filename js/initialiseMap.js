2a9ce9b33c62307d9ccaf23ee420d3e5b6136b77
William Tetlow <tjb12161@uni.strath.ac.uk>
2015-04-20 10:03:13 +0100

    Maps functionality
    Maps so far. Maps is able to load current location and also a specified
    location. Beginning of code for getting farms from database is also
    there.

--- js/initialiseMap.js
+++ js/initialiseMap.js
@@ -2,12 +2,28 @@
  * Function to set the properties of the 
  */
 
+/*jslint node: true, browser: true */
+"use strict";
+
 function initialise() {
-  var mapProp = {
-    center:new google.maps.LatLng(55.8599764,-4.2511268),
-    zoom:14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  window.map=new google.maps.Map(document.getElementById("mapDiv"),mapProp); /*Create map object in div*/
+    var mapProp = {
+        center: new google.maps.LatLng(55.8599764, -4.2511268),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    },
+        updatePosition = function(position) {
+            mapProp = {
+                center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                zoom: 14,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+        },
+        getLocation = function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(updatePosition);
+            }
+        };
+    getLocation();
+    window.map = new google.maps.Map(document.getElementById("mapDiv"), mapProp); /*Create map object in div*/
 }
 google.maps.event.addDomListener(window, 'load', initialise); /*Calls initialise function upon load*/
--- js/searchMap.js
+++ js/searchMap.js
@@ -4,22 +4,53 @@
  * and open the template in the editor.
  */
 
-function searchMap(){
-    var postcodeInputElement = document.getElementById("postcodeInput");
-    var searchTerm = postcodeInputElement.value;
-    var  geocoder = new google.maps.Geocoder();
-    searchTerm += ", UK";
-    console.log(searchTerm);
-    geocoder.geocode({ 'address': searchTerm}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            var location = results[0].geometry.location;
-            map.setCenter(location);
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*jslint node: true, browser: true */
+"use strict";
+
+function searchMap() {
+    var moveLocation = function () {
+        var postcodeInputElement = document.getElementById("postcodeInput"),
+            searchTerm = postcodeInputElement.value,
+            geocoder = new google.maps.Geocoder();
+
+        searchTerm += ", UK";
+        geocoder.geocode({ 'address': searchTerm}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var location = results[0].geometry.location;
+                window.map.setCenter(location);
             //map.setZoom(15);
-          } else {
-            alert("Geocode was not successful for the following reason: " + status);
-          }
+            } else {
+                postcodeInputElement.value = "Invalid Postcode";
+            }
         });
-      
+    },
+        checkForReturn = function (e) {
+            if (e.keyCode === 13) {
+                moveLocation();
+            }
+        },
+        talkToDB = function (e) {
+            var xmlhttp;
+            if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {// code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            var x = xmlhttp.open("GET", "getFarms.php", true);
+            xmlhttp.send();
+        };
+
+    var button = document.getElementById("clickPostCode");
+    button.addEventListener("click", moveLocation);
+    document.getElementById("postcodeInput").addEventListener("keydown", checkForReturn, false);
     
 }
+
+
 
