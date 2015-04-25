42f868c6058009de7d8d5fa081fc80638ec30b63
Nightpaws <nightpaws@hotmail.co.uk>
2015-04-25 21:17:24 +0100

    Login & Reg Forms
    Completed Login & Reg forms with client side input validation and JQuery
    transitioning between forms. CSS to place them was a little hack and
    slash due to wonky surrounding formatting. Committing JQuery code
    temporarily to work from for testing purposes and because google's was
    throwing errors in Chrome

---
+++ js/loginReg.js
@@ -0,0 +1,101 @@
+function showRegister() {
+    console.log("Show Register - clicked!");
+    // $('input').val('');
+    $('#registerPg').slideDown();
+    $('#loginPg').slideUp();
+
+};
+
+function hideRegister() {
+    console.log("Hide Register - clicked!");
+    // $('input').val('');
+    $('#loginPg').slideDown();
+    $('#registerPg').slideUp();
+
+};
+
+function confirmRegister() {
+    var username = document.getElementById("uname").value;
+    var password = document.getElementById("passwd").value;
+    var email = document.getElementById("email").value;
+    console.log("Confirm Register - clicked!");
+    console.log("username is: " + username);
+    console.log("password is: " + password);
+    console.log("email is: " + email);
+    validateRegForm();
+    console.log("Registration JS Done!");
+};
+
+function validateLogin() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log("Validate Login - clicked!");
+    console.log("username is: " + username);
+    console.log("password is: " + password);
+    validateLoginForm();
+    console.log("Login JS Done!");
+};
+
+$(document).ready(function() {
+    console.log("Document ready!");
+    $("#registerBtn").click(showRegister);
+    $("#cancelBtn").click(hideRegister);
+    $("#confirmRegBtn").click(confirmRegister);
+    $("#loginBtn").click(validateLogin);
+});
+
+
+function validateLoginForm() {
+    // Username Validation
+    var x = document.getElementById("username").value;
+    if (x == null || x == "") {
+        alert("Username field cannot be left blank");
+        return false;
+    }
+    if (x.length > 59) {
+        alert("Your username is too long, it needs to be less than 60 characters in length")
+        return false;
+    }
+    // Password Validation
+
+    var x = document.getElementById("password").value;
+    if (x == null || x == "") {
+        alert("Password field cannot be left blank");
+        return false;
+    }
+    if (x.length > 127) {
+        alert("Your password is too long, it needs to be less than 128 characters in length")
+        return false;
+    }
+};
+
+function validateRegForm() {
+    // Username Validation
+    var x = document.getElementById("uname").value;
+    if (x == null || x == "") {
+        alert("Username field cannot be left blank");
+        return false;
+    }
+    if (x.length > 59) {
+        alert("Your username is too long, it needs to be less than 60 characters in length")
+        return false;
+    }
+    // Email validation
+    var x = document.getElementById("email").value;
+    var emailStructure = /\S+@\S+\.\S+/; //this is the very basic structure an email address should follow
+
+    if (!(emailStructure.test(x))) {
+        alert("That doesn't look like a valid email address, it should look like:\nYouremail@example.com");
+        return false;
+    }
+    // Password Validation
+    var x = document.getElementById("passwd").value;
+    if (x == null || x == "") {
+        alert("Password field cannot be left blank");
+        return false;
+    }
+    if (x.length > 127) {
+        alert("Your password is too long, it needs to be less than 128 characters in length")
+        return false;
+    }
+};
